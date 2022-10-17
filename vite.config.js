b32076a15960007bbd56abe86bb31c7fc91fe8a1
unknown <grisi.gav@gmail.com>
2022-10-16 23:43:57 -0500

    Fix BOX

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://GRISEDAACHO.github.io/gh-react-vite',
+  base: 'https://GRISELDAACHO.github.io/gh-react-vite',
   plugins: [react()]
 })
